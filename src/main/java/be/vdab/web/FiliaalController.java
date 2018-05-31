b036116bdaa0183e9f029cb939663a729540d762
bobl <git@bttn.be>
2018-05-31 17:43:48 +0200

    junit & request parameters

--- src/main/java/be/vdab/web/FiliaalController.java
+++ src/main/java/be/vdab/web/FiliaalController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import be.vdab.services.FiliaalService;
 
@@ -15,6 +16,7 @@ class FiliaalController {
 	private static final String FILIALEN_VIEW = "filialen/filialen";
 	private static final String TOEVOEGEN_VIEW = "filialen/toevoegen";
 	private static final String REDIRECT_URL_NA_TOEVOEGEN = "redirect:/filialen";
+	private static final String FILIAAL_VIEW = "filialen/filiaal";
 	private static final Logger LOGGER = Logger.getLogger(FiliaalController.class.getName());
 	private final FiliaalService filiaalService;
 	
@@ -24,8 +26,8 @@ class FiliaalController {
 	}
 
 	@GetMapping
-	String findAll() {
-		return FILIALEN_VIEW;
+	ModelAndView findAll() {
+		return new ModelAndView(FILIALEN_VIEW, "filialen", filiaalService.findAll());
 	}
 
 	@GetMapping("toevoegen")
@@ -37,5 +39,12 @@ class FiliaalController {
 	String create() {
 	 LOGGER.info("filiaal record toevoegen aan database");
 	 return REDIRECT_URL_NA_TOEVOEGEN;
+	}
+	
+	@GetMapping(params = "id") 
+	ModelAndView read(long id) { 
+		ModelAndView modelAndView = new ModelAndView(FILIAAL_VIEW);
+		filiaalService.read(id).ifPresent(filiaal -> modelAndView.addObject(filiaal)); 
+		return modelAndView;
 	}
 }
